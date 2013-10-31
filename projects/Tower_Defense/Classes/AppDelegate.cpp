7e1a1d315dcde8c22146b706aff6546e1cfb8d0c
jhao <382866657@qq.com>
2013-10-31 18:59:39 +0800

    set search path

--- projects/Tower_Defense/Classes/AppDelegate.cpp
+++ projects/Tower_Defense/Classes/AppDelegate.cpp
@@ -35,9 +35,15 @@ bool AppDelegate::applicationDidFinishLaunching()
     // turn on display FPS
     pDirector->setDisplayStats(true);
     
+    vector<string>searchPath;
+    
+    searchPath.push_back("res");
+    
+    CCFileUtils::sharedFileUtils()->setSearchPaths(searchPath);
+    
     // set FPS. the default value is 1.0/60 if you don't call this
     pDirector->setAnimationInterval(1.0 / 60);
-    
+
     ScriptingCore* sc = ScriptingCore::getInstance();
     sc->addRegisterCallback(register_all_cocos2dx);
     sc->addRegisterCallback(register_all_cocos2dx_extension);
